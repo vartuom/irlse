26a2fe42b99da07bc7f547451a3472b7f72ab830
vartuom <v.artuom@ya.ru>
2023-06-30 23:00:14 +0800

    init

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Golos_Text } from 'next/font/google'
+import Header from '@/components/Header/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const golos = Golos_Text({ subsets: ['latin', 'cyrillic'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={golos.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
